7d93a55f23ea513597853b364c1c6316204306d0
Mattias Sattel <matthias.sattel@cgm.com>
2016-03-01 23:10:42 +0100

    add a first icon

--- src/cljs/kraken/connection_component.cljs
+++ src/cljs/kraken/connection_component.cljs
@@ -13,15 +13,16 @@
     (render-state [this {:keys [delete]}]
       (let [label (str (:label data))
             class-label (str "connection-" label)]
-      (html
-       [:div {:class (str "pure-u-1 pure-u-md-1-3 pure-u-lg-1-5 connection-tile " class-label)}
-        [:div {:class "pure-u-g connection-list-item"}
-         [:div {:class "pure-u-1 pure-u-md-1-2"}
-          [:h3 {:class "connection-list-item-header"}
-           label]]
-         [:div {:class "pure-u-1 pure-u-md-1-2"}
-          [:button {:class "pure-button" :onClick (fn [e] (put! delete @data))}"Delete it"]]]]
-       ))
+        (html
+         [:div {:class (str "pure-u-1 pure-u-md-1-3 pure-u-lg-1-5 connection-tile " class-label)}
+          [:div {:class "pure-u-g connection-list-item"}
+           [:div {:class "pure-u-1 pure-u-md-1-2"}
+            [:h3 {:class "connection-list-item-header"}
+             label]]
+           [:div {:class "pure-u-1 pure-u-md-1-2"}
+            [:button {:class "pure-button" :onClick (fn [e] (put! delete @data))}
+             [:i {:class "fa fa-trash"}]]]]]
+         ))
       )))
 
 
@@ -31,37 +32,37 @@
 
 (defn connection-form [data owner]
   (html [:div
-                    [:form {:class "pure-form pure-form-aligned"}
-                     [:div {:class "pure-control-group"}
-                      [:label {:for="type"} "Type"]
-                      [:select {:id "type" :onChange (fn [e] (let [el (sel1 :#type)]
-                                                              (.log js/console
-                                                                    (str (.-value (aget (.-options el) (.-selectedIndex el)))))
-                                                              ))}
-                       [:option "PostgreSql"]
-                       [:option "SqlServer"]
-                       [:option "Mongo"]]]
-                     [:div {:class "pure-control-group"}
-                      [:label {:for="label"} "Label"]
-                      [:input {:type "text" :id "new-connection-label"}]]
-                     [:div {:class "pure-control-group"}
-                      [:label {:for="host"} "Hostname"]
-                      [:input {:type "text" :id "new-connection-host"}]]
-                     [:div {:class "pure-control-group"}
-                      [:label {:for="port"} "Port"]
-                      [:input {:type "text" :id "new-connection-port"}]]
-                     [:div {:class "pure-controls"}
-                      [:button {:class "pure-button" :type "button" :onClick (fn [e]
-                                                                               (let [el (sel1 :#type)
-                                                                                     new-connection {:label (read-string (dommy/value (sel1 :#new-connection-label)))
-                                                                                                     :host (read-string (dommy/value (sel1 :#new-connection-host)))
-                                                                                                     :port (read-string (dommy/value (sel1 :#new-connection-port)))
-                                                                                                     :type (str (.-value (aget (.-options el) (.-selectedIndex el))))}]
-                                                                                 (.log js/console (str new-connection))
-                                                                                 (add-connection data owner new-connection)
-                                                                                 ))
-                                } "Add Connection"]]]
-                    ]))
+         [:form {:class "pure-form pure-form-aligned"}
+          [:div {:class "pure-control-group"}
+           [:label {:for="type"} "Type"]
+           [:select {:id "type" :onChange (fn [e] (let [el (sel1 :#type)]
+                                                    (.log js/console
+                                                          (str (.-value (aget (.-options el) (.-selectedIndex el)))))
+                                                    ))}
+            [:option "PostgreSql"]
+            [:option "SqlServer"]
+            [:option "Mongo"]]]
+          [:div {:class "pure-control-group"}
+           [:label {:for="label"} "Label"]
+           [:input {:type "text" :id "new-connection-label"}]]
+          [:div {:class "pure-control-group"}
+           [:label {:for="host"} "Hostname"]
+           [:input {:type "text" :id "new-connection-host"}]]
+          [:div {:class "pure-control-group"}
+           [:label {:for="port"} "Port"]
+           [:input {:type "text" :id "new-connection-port"}]]
+          [:div {:class "pure-controls"}
+           [:button {:class "pure-button" :type "button" :onClick (fn [e]
+                                                                    (let [el (sel1 :#type)
+                                                                          new-connection {:label (str (dommy/value (sel1 :#new-connection-label)))
+                                                                                          :host (str (dommy/value (sel1 :#new-connection-host)))
+                                                                                          :port (str (dommy/value (sel1 :#new-connection-port)))
+                                                                                          :type (str (.-value (aget (.-options el) (.-selectedIndex el))))}]
+                                                                      (.log js/console (str new-connection))
+                                                                      (add-connection data owner new-connection)
+                                                                      ))
+                     } "Add Connection"]]]
+         ]))
 
 (defn connections [data owner]
   (reify
@@ -79,11 +80,10 @@
     om/IRenderState
     (render-state [this {:keys [delete]}]
       (html [:div {:class "connections-list"}
-                    (om/build-all view (:connections data)
-                                  {:init-state {:delete delete}})
+             (om/build-all view (:connections data)
+                           {:init-state {:delete delete}})
              (connection-form data owner)]
             )
       
       )
     ))
-
